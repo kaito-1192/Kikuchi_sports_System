7dbba7231691df88b4deacdea53aa8d37c2677f2
STD\s20217043 <20217043-kikuchikaito@hcs.ac.jp>
2022-11-11 14:58:36 +0900

    会員番号表示処理保留

--- Kikushi_sports_System/Form2.Designer.cs
+++ Kikushi_sports_System/Form2.Designer.cs
@@ -34,6 +34,8 @@ namespace Kikushi_sports_System
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -62,8 +64,9 @@ namespace Kikushi_sports_System
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(162, 101);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "会員検索";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -83,11 +86,22 @@ namespace Kikushi_sports_System
             this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = true;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(614, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(174, 107);
+            this.dataGridView1.TabIndex = 5;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -95,6 +109,8 @@ namespace Kikushi_sports_System
             this.Controls.Add(this.button1);
             this.Name = "Form2";
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -106,5 +122,6 @@ namespace Kikushi_sports_System
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Kikushi_sports_System/Form2.cs
+++ Kikushi_sports_System/Form2.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace Kikushi_sports_System
 {
@@ -27,9 +28,30 @@ namespace Kikushi_sports_System
 
         private void button2_Click(object sender, EventArgs e)
         {
+            //form3を取得
             Form3 form3 = new Form3();
+            //form3を表示
             form3.Show();
+            //form2を非表示
             this.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            using (SQLiteConnection con = new SQLiteConnection("Data Source=m_table.db"))
+            {
+                // DataTableを生成します。
+                var dataTable = new DataTable();
+                // SQLの実行
+                var adapter = new SQLiteDataAdapter("SELECT MAX(CD) as 会員番号 FROM t_product", con);
+                adapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
         }
     }
 }
